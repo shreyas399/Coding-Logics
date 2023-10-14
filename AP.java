e8dd98de07498b6a54ab923de2d974cb706dde88
shreyas399 <129728792+shreyas399@users.noreply.github.com>
2023-10-14 14:05:16 +0530

    Create AP.java

---
+++ AP.java
@@ -0,0 +1,14 @@
+class demo
+{
+  public static void main(String []args)
+  {
+    int N=4,
+    A1=2,
+    A2=3;
+
+    int diff=A2-A1;
+    int ap=(A1+(N-1)*diff);
+   System.out.println(ap);
+
+  }
+}
